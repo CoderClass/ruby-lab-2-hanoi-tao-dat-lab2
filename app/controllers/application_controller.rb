6e91068fc691676e10a0845809e5e6762fa09c94
Tao Nhu <%%GITEMAIL%%>
2016-10-18 20:54:47 +0700

    milestone 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user, :signed_in?
+  helper_method :current_user, :signed_in?, :require_login
 
   protected
+
+  def require_login
+    unless signed_in?
+      flash[:error] = 'You must sign in to see this page!'
+      redirect_to login_path
+    end
+  end
+
+  def skip_if_logged_in
+    if signed_in?
+      redirect_to users_path
+    end
+  end
 
   def signed_in?
     current_user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_login, except: [:new]
+  before_action :skip_if_logged_in, except: [:index, :create]
 
   def new
     @user = User.new
