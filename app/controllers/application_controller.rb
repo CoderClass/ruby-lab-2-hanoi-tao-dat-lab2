820d58bc4c5a9f5ddd625c2374eadc818a2abef5
Tao Nhu <%%GITEMAIL%%>
2016-10-18 20:36:52 +0700

    Milestone 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :signed_in?
 
   protected
+
+  def signed_in?
+    current_user
+  end
 
   def current_user
    return @current_user if @current_user
